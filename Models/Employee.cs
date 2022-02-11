b3d2c99196b20caf6401da9bdf48f8e24a5a301f
Abhinav Jaiju <abhinav.jaiju@experionglobal.com>
2022-02-11 15:47:39 +0530

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeApp.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Designation { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public int DepartmentId { get; set; }
+        public bool InService { get; set; }
+    }
+}
